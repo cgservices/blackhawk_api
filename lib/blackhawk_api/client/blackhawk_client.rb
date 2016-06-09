2eb4fee723d0b1d844d90c77f2a823a58213b321
Luis Doubrava <ldoubrava@gmail.com>
2016-06-09 09:48:46 +0200

    small fixes and changes to specs

--- lib/blackhawk_api/client/blackhawk_client.rb
+++ lib/blackhawk_api/client/blackhawk_client.rb
@@ -7,7 +7,7 @@ module BlackhawkApi
       catalog_service = BlackhawkApi::CatalogService.new
       product_service = BlackhawkApi::ProductService.new
       available_catalogs = []
-      
+
       catalogs = catalog_service.all BlackhawkApi::Requests::GetAllCatalogsRequest.new
       catalogs.results.each do |c|
         catalog_details = []
@@ -16,25 +16,25 @@ module BlackhawkApi
         catalog.details.product_urls.each do |url|
           ids << BlackhawkApi::IdentityExtractor.to_identity(url).to_s
         end
-        
+
         ids.each do |id|
           product = product_service.find id
           catalog_details << product
         end
-        
+
         available_catalogs << catalog_details
       end
       available_catalogs
     end
-    
+
     # Reads product details for a single product.
     # @param product_id The identifier of the product.
     # @return The product details.
     def read_product product_id
       product_service = BlackhawkApi::ProductService.new
-      product_service.find product_id      
+      product_service.find product_id
     end
-    
+
     # Reads account details for a single account
     # @param account_id The identifier of the account.
     # @return The account details.
@@ -42,7 +42,7 @@ module BlackhawkApi
       account_service = BlackhawkApi::AccountService.new
       account_service.find account_id
     end
-    
+
     # Reads transaction details for a single transaction.
     # @param transaction_id The identifier of the transaction.
     # @return The transaction details.
@@ -50,17 +50,17 @@ module BlackhawkApi
       transaction_service = BlackhawkApi::TransactionService.new
       transaction_service.find transaction_id
     end
-    
+
     # Finds transactions for the specified account.
     # @param account_id The identifier of the account.
     # @return All transaction details for the account.
     def read_transactions_for_account account_id
       transaction_service = BlackhawkApi::TransactionService.new
       request = BlackhawkApi::Requests::FindAccountTransactionsByAccountIdRequest.new(account_id)
-      
+
       transaction_service.find_by_account_id request
     end
-    
+
     # Generates an egift for the specified product configuration.
     # @param product_config_id The identifier for the product configuration.
     # @param amount The requested value for the egift.
@@ -68,26 +68,26 @@ module BlackhawkApi
     # @return The EGift details.
     def generate_egift product_config_id, amount, reference
       gift_service = BlackhawkApi::GiftService.new
-      
+
       request = BlackhawkApi::Requests::GenerateGiftCardRequest.new(
         nil, nil, nil, amount, nil, nil, reference,
         nil, product_config_id, nil, nil
       )
-      
+
       gift_service.generate request
     end
-    
+
     # Reverses a generated egift or a voided egift.
     # @param request_id The identifier used during the initial request.
     # @return The account transaction details.
     def reverse_egift request_id
       gift_service = BlackhawkApi::GiftService.new
-      
+
       request = BlackhawkApi::Requests::ReverseGiftCardRequest.new(request_id)
-      
+
       gift_service.reverse request
     end
-    
+
     # Voids a generated egift.
     # @param request_id The identifier used during the initial request.
     # @param egift_id The identifier given to the generated egift.
@@ -95,10 +95,10 @@ module BlackhawkApi
     # @return The EGift details
     def void_egift egift_id, reference
       gift_service = BlackhawkApi::GiftService.new
-      
+
       request = BlackhawkApi::Requests::VoidGiftCardRequest.new(
         egift_id, reference)
-      
+
       gift_service.void request
     end
   end
--- spec/integration/blackhawk_certification_test_script_spec.rb
+++ spec/integration/blackhawk_certification_test_script_spec.rb
@@ -7,20 +7,20 @@ describe BlackhawkApi do
       it 'should call read account with the account id from the eGift json and return account information: account number, security code, cached balance, currency and product line id' do
         valid_account_id = 'J5FZ4KAC98LALH5WC9GHA47DDC'.freeze
         service = BlackhawkApi::AccountService.new
-        
+
         result = service.find(valid_account_id)
-        
+
         expect(result.code).to eq(200)
         expect(result.information).not_to eq(nil)
         expect(result.information.type).to eq("GIFT_CARD")
         expect(result.information.status).to eq("ACTIVATED")
         expect(result.information.product_id).not_to eq(nil)
       end
-      
+
       it 'should call read account with an invalid account id from the eGift json and return 404 - invalid id, 400 - bad request' do
         invalid_account_id =  '000000AC9000000000000471'.freeze
         service = BlackhawkApi::AccountService.new
-        
+
         expect {
           service.find(invalid_account_id)
         }.to raise_error { | error |
@@ -31,72 +31,72 @@ describe BlackhawkApi do
       end
     end
   end
-  
+
   context 'Product Catalog Management', :product_catalog => true do
     describe 'Read Product Catalog' do
       VALID_CATALOG_ID = 'R7N2G9WKC9CKHJ5FSS37T41RMH'.freeze
       INVALID_CATALOG_ID = '123456789012'.freeze
-      
+
       it 'should Call API with catalog ID and return product ids of products within the catalog' do
         service = BlackhawkApi::CatalogService.new
-        
+
         result = service.find(VALID_CATALOG_ID)
 
         expect(result.code).to eq(200)
         expect(result.details).not_to eq(nil)
         expect(result.details.product_urls.length).to be > 0
       end
-      
+
       it 'should call API with invalid Catalog ID and return 404 - invalid id, 400 - bad request' do
         service = BlackhawkApi::CatalogService.new
-        
+
         expect {
-          service.find(INVALID_CATALOG_ID) 
-        }.to raise_error(BlackhawkApi::ApiError)        
+          service.find(INVALID_CATALOG_ID)
+        }.to raise_error(BlackhawkApi::ApiError)
       end
     end
-    
+
     describe 'Get Product Catalog Version' do
       it 'should call API with valid Catalog ID of test case 1 and return the product catalog version' do
         service = BlackhawkApi::CatalogService.new
-        
+
         result = service.get_version(VALID_CATALOG_ID)
-        
+
         expect(result.code).to eq(200)
         expect(result.version.to_i).to be > 0
       end
     end
-    
+
     describe 'Query all product catalogs' do
       it 'should query all product catalogs with relevant info and return the ID, version and name of one catalog' do
         service = BlackhawkApi::CatalogService.new
-        
+
         result = service.all
-        
+
         expect(result.code).to eq(200)
         expect(result.results.length).to be 1
         expect(result.results[0].entity_id.length).to eq(26)
         expect(result.results[0].version.to_i).to be > 0
         expect(result.results[0].name.length).to be > 0
       end
-      
+
       it 'should query product catalog with an invalid element and return 400 - invalid attributes' do
         skip
       end
     end
-    
+
     describe 'Product Management' do
       it 'should handle the return of Service not Available or Bad Gateway' do
         skip
       end
     end
-    
+
     describe 'Query all Product Catalogs' do
       it 'should call API with no input and return the ID, version and name of 1 catalog' do
         service = BlackhawkApi::CatalogService.new
-        
+
         result = service.all
-        
+
         expect(result.code).to eq(200)
         expect(result.results.length).to be > 0
         expect(result.results[0].entity_id.length).to eq(26)
@@ -105,27 +105,27 @@ describe BlackhawkApi do
       end
     end
   end
-  
+
   context 'Product Management', :product_management => true do
     VALID_PRODUCT_ID = 'WH7V1Z5584XM0XGZ7JS61C7FHW'
     INVALID_PRODUCT_ID = '123456789012'
-    
+
     describe 'Read Product' do
       it 'should read product with the product id in catalog and return valid json' do
         service = BlackhawkApi::ProductService.new
-        
+
         result = service.find VALID_PRODUCT_ID
-        
+
         expect(result.code).to eq(200)
         expect(result.summary).not_to eq(nil)
         expect(result.summary.product_name.length).to be > 0
         expect(result.details).not_to eq(nil)
         expect(result.details.product_configurations).not_to eq(nil)
       end
-      
+
       it 'should read product with a bad product id and handle a 404 - invalid id, 400 - Bad Request' do
         service = BlackhawkApi::ProductService.new
-        
+
         expect {
           service.find INVALID_PRODUCT_ID
         }.to raise_error(BlackhawkApi::ApiError)
@@ -136,7 +136,7 @@ describe BlackhawkApi do
       it 'should return json with the list of summary information about a subset of the product that meets the specified criteria and cursor view parameters' do
         skip
       end
-      
+
       it 'should call query with an invalid element and return 400 - invalid attributes' do
         skip
       end
@@ -147,107 +147,107 @@ describe BlackhawkApi do
         service = BlackhawkApi::ProductService.new
         expect {
           service.find '8MJAB7C7P6NZ7YAH8P6N7W2NTL'
-        }.to raise_error(BlackhawkApi::ApiError)        
+        }.to raise_error(BlackhawkApi::ApiError)
       end
     end
   end
-  
+
   context 'eGift Processing', :egift_processing => true do
     describe 'Generate eGift' do
-      VALID_CONFIG_ID = 'SNPZWSWTQHWYCF4THKMPFK5ZH0'.freeze
+      VALID_CONFIG_ID = '8MJAB7C7P6NZ7YAH8P6N7W2NTL'.freeze
       INVALID_CONFIG_ID = '123456789012'.freeze
       OTHER_CONFIG_ID = 'G6679F4ZG3ASQXMGAGT9C8FBK4'.freeze
-      
+
       it 'should be called with a valid configuration id and return a valid eGift json: 1' do
         # VGQ4ZMS0X1JHZ7LAXQVGRT8QZ9: DIGITAL FRC 01A: APPROVED FIXED (BARCODE) - NL EUR 5.0
         # Config: SNPZWSWTQHWYCF4THKMPFK5ZH0
         amount = 5
         reference = rand.to_s[2..13]
         client = BlackhawkApi::BlackhawkClient.new
-        
+
         result = client.generate_egift(VALID_CONFIG_ID, amount, reference)
-        
+
         expect(result.code).to eq(200)
         expect(result.information).not_to be(nil)
         expect(result.information.gift_amount).to eq(amount)
         expect(result.information.status).to eq('ACTIVATED')
         expect(result.information.suspended).to eq(false)
-        expect(result.information.product_configuration_id).to eq(VALID_CONFIG_ID)        
+        expect(result.information.product_configuration_id).to eq(VALID_CONFIG_ID)
       end
-      
+
       it 'should be called with invalid amount and return 400 giftamount null: 1.1', :test => true do
         INVALID_AMOUNT_CONFIG_ID = 'PZF9A147MCQV49PD6MWHMZ1109'
         amount = nil
         reference = rand.to_s[2..13]
         client = BlackhawkApi::BlackhawkClient.new
-        
+
         expect {
-          client.generate_egift(VALID_CONFIG_ID, amount, reference)
+          client.generate_egift(INVALID_AMOUNT_CONFIG_ID, amount, reference)
         }.to raise_error(BlackhawkApi::ApiError)
       end
-      
+
       it 'should be called with an invalid purchase id and return purchaserId null: 1.2' do
         amount = 5
         reference = rand.to_s[2..13]
         invalid_purchaser_id = '6A6GRHWP2GNW71S67W2AX41FS'
         gift_service = BlackhawkApi::GiftService.new
-        
+
         request = BlackhawkApi::Requests::GenerateGiftCardRequest.new(
           nil, nil, nil, amount, invalid_purchaser_id, nil, reference,
           nil, VALID_CONFIG_ID, nil, nil
         )
         expect {
           gift_service.generate request
-        }.to raise_error(BlackhawkApi::ApiError)        
+        }.to raise_error(BlackhawkApi::ApiError)
       end
-      
+
       it 'should be called with null configuration id and return productConfigurationId.null: 1.3' do
         pending('Not possible yet due to argument validations')
         fail
-        
+
         amount = 5
         reference = rand.to_s[2..13]
         client = BlackhawkApi::BlackhawkClient.new
-        
+
         expect {
           client.generate_egift(nil, amount, reference)
         }.to raise_error(BlackhawkApi::ApiError)
       end
-      
+
       it 'should be called with invalid configuration id and return 409 product not found: 1.4' do
         amount = 5
         reference = rand.to_s[2..13]
         client = BlackhawkApi::BlackhawkClient.new
-        
+
         expect {
           client.generate_egift(INVALID_CONFIG_ID, amount, reference)
         }.to raise_error(BlackhawkApi::ApiError)
       end
-      
+
       it 'should be called while not part of the clients configuration and return 409 product not in catalog: 1.5' do
         amount = 5
         reference = rand.to_s[2..13]
         client = BlackhawkApi::BlackhawkClient.new
-        
+
         expect {
           client.generate_egift(OTHER_CONFIG_ID, amount, reference)
         }.to raise_error(BlackhawkApi::ApiError)
       end
-      
+
       it 'should be called with invalid value and return 409 account creation failed: 1.6' do
         amount = -1
         reference = rand.to_s[2..13]
         client = BlackhawkApi::BlackhawkClient.new
-        
+
         expect {
           client.generate_egift(VALID_CONFIG_ID, amount, reference)
         }.to raise_error(BlackhawkApi::ApiError)
       end
-      
+
       it 'should be called when the service is unavailable and return Service Not Available / Bad Gateway: 1.8' do
         # 4ZQ8DP8PBNTNL7KH5ZRGYGV435: DIGITAL FRC 07: 502 ERROR (30 SEC T/O) - NL EUR 5.0
         # Config: 2KHHTTQ67ZMDL3CTKFL3Q563L6
-        FRC_CONFIG = '2KHHTTQ67ZMDL3CTKFL3Q563L6' 
+        FRC_CONFIG = '2KHHTTQ67ZMDL3CTKFL3Q563L6'
         gift_service = BlackhawkApi::GiftService.new
         amount = 5
         ref = rand.to_s[2..13]
@@ -255,15 +255,15 @@ describe BlackhawkApi do
         request = BlackhawkApi::Requests::GenerateGiftCardRequest.new(
           nil, nil, nil, amount, nil, nil, ref,
           nil, FRC_CONFIG, nil, nil)
-        
+
         expect { gift_service.generate request }.to raise_error { | error |
         }
       end
-      
+
       it 'should be called when Blast is down and return timeout with the processor: 1.9' do
         skip
       end
-      
+
       it 'should be called when product is not available and return product inventory is not available: 1.10', :inventory => true do
         # 7Y77APSD5Q52FVQAYCXN45SLZF: DIGITAL FRC 02: 409 CARD NOT FOUND - NL EUR 5.0
         # Config: SG49BRMV9W0PV3J1NQLG8NKQ2A
@@ -276,14 +276,14 @@ describe BlackhawkApi do
         request = BlackhawkApi::Requests::GenerateGiftCardRequest.new(
           nil, nil, nil, amount, nil, nil, ref,
           nil, FRC06_CONFIG, nil, nil)
-        
+
         expect { gift_service.generate request }.to raise_error { | error |
           expect(error.http_code).to eq(409)
-          expect(error.error_code).to eq('egiftprocessing.account.creation.failed.account.inventory.unavailable')
+          expect(error.error_code).to eq('egiftprocessing.account.creation.failed.card.not.found')
         }
       end
     end
-    
+
     describe 'Void eGift', :void_egift => true do
       it 'should be called with a valid egift id and return a valid eGift json: 3' do
         client = BlackhawkApi::BlackhawkClient.new
@@ -291,9 +291,9 @@ describe BlackhawkApi do
         amount = 5
         egift = client.generate_egift('8MJAB7C7P6NZ7YAH8P6N7W2NTL', amount, reference)
         egift_id = BlackhawkApi::IdentityExtractor.to_identity(egift.information.entity_id)
-        
+
         result = client.void_egift(egift_id, reference)
-        
+
         expect(result.code).to eq(200)
         expect(result.information).not_to eq(nil)
         expect(result.information.gift_amount).to eq(amount)
@@ -301,19 +301,19 @@ describe BlackhawkApi do
         expect(result.information.status).to eq("ACTIVATED")
         expect(result.information.suspended).to eq(false)
       end
-      
+
       it 'should be called with an invalid egift id and return egift not found: 3.1' do
         client = BlackhawkApi::BlackhawkClient.new
         reference = rand.to_s[2..13]
         amount = 5
         INVALID_EGIFT_ID = '00100001000000100001010111'
         egift = client.generate_egift('8MJAB7C7P6NZ7YAH8P6N7W2NTL', amount, reference)
-        
+
         expect {
           client.void_egift(INVALID_EGIFT_ID, reference)
         }.to raise_error(BlackhawkApi::ApiError)
       end
-      
+
       it 'should be called when eGift is already voided OR underlying account is already closed and return egift already voided: 3.2' do
         client = BlackhawkApi::BlackhawkClient.new
         reference = rand.to_s[2..13]
@@ -323,13 +323,13 @@ describe BlackhawkApi do
         id = BlackhawkApi::IdentityExtractor.to_identity(egift.information.entity_id)
         # Perform initial void
         client.void_egift(id, reference)
-        
+
         expect {
           client.void_egift(id, reference)
         }.to raise_error(BlackhawkApi::ApiError)
       end
     end
-    
+
     describe 'eGift Processing' do
       it 'should be called when the service is unavailable and return Service Not Available or Bad Gateway: 4' do
         # 4ZQ8DP8PBNTNL7KH5ZRGYGV435: DIGITAL FRC 07: 502 ERROR (30 SEC T/O) - NL EUR 5.0
@@ -342,7 +342,7 @@ describe BlackhawkApi do
           client.generate_egift('2KHHTTQ67ZMDL3CTKFL3Q563L6', amount, reference)
         }.to raise_error(BlackhawkApi::ApiError)
       end
-      
+
       it 'should be called with a timeout and return ws.provider.timeout: 4.1' do
         # BAP0S82F351Q628MSKXTZ0R726: DIGITAL FRC 08: 504 GATEWAY TIMEOUT - NL EUR 5.0
         # Config: 73NYTS7APGW5GT5BMM4RZC1PJ9
@@ -353,6 +353,29 @@ describe BlackhawkApi do
         expect {
           client.generate_egift('73NYTS7APGW5GT5BMM4RZC1PJ9', amount, reference)
         }.to raise_error(BlackhawkApi::ApiError)
+      end
+    end
+
+    describe 'eGift idempotency' do
+      it 'should resend the request 3 times incrementing previousAttempts' do
+        # BAP0S82F351Q628MSKXTZ0R726: DIGITAL FRC 08: 504 GATEWAY TIMEOUT - NL EUR 5.0
+        # Config: 73NYTS7APGW5GT5BMM4RZC1PJ9
+        client = BlackhawkApi::BlackhawkClient.new
+        # reference = 'REF0909090001'
+        # amount = 5
+        # tries = 0
+        # egift = client.generate_egift('SNPZWSWTQHWYCF4THKMPFK5ZH0', amount, reference)
+        # egift_id = BlackhawkApi::IdentityExtractor.to_identity(egift.information.entity_id)
+        # sleep(2)
+        result = client.reverse_egift('635346958583')
+        # 409
+        # {"errorCode":"original.transaction.not.found","message":"Business Exception occurred while performing the operation"}18
+        expect(result.code).to eq(200)
+        # expect(result.information).not_to eq(nil)
+        # expect(result.information.gift_amount).to eq(amount)
+        # expect(result.information.product_configuration_id).to eq(VALID_CONFIG_ID)
+        # expect(result.information.status).to eq('ACTIVATED')
+        # expect(result.information.suspended).to eq(false)
       end
     end
   end
